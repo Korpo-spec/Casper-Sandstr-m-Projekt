57f81b104c977784c13f2e0980734f0dde9be949
Korpo-spec <casper.sandstrom@elev.ga.ntig.se>
2020-01-30 11:01:26 +0100

    Initial commit

---
+++ Javascript/Enemy.js
@@ -0,0 +1,80 @@
+class Enemy{
+    constructor(position, health, movementSpeed, path){
+        this.position = position;
+        this.health = health;
+        this.movementSpeed = movementSpeed;
+        this.path = path;
+        
+        
+        
+        if (Math.abs(path[0][1]) == 1) {
+            this.XChanger = path[0][1];
+        }
+        else{
+            this.XChanger = 0;
+        }
+        if (Math.abs(path[0][1]) == 2) {
+            this.YChanger = path[0][1]/2;
+        }
+        else{
+            this.YChanger = 0
+        }
+        
+        
+        this.currentPathPoint = 0;
+    }
+
+    clearDraw() {
+        context.clearRect(this.position[0]*10 - 2, this.position[1]*10 - 2, 101, 101);
+    }
+
+    Draw(division) {
+        
+        
+        
+        
+        
+        if (this.XChanger != 0 )  {
+            
+            if (this.position[0] > this.path[this.currentPathPoint ][0] && this.XChanger > 0) {
+                
+                this.YChanger = path[this.currentPathPoint + 1][1] / -2;
+                
+                this.XChanger = 0;
+                this.currentPathPoint += 1;
+            }
+            else if (this.position[0] < this.path[this.currentPathPoint ][0] && this.XChanger < 0) {
+                
+                this.YChanger = path[this.currentPathPoint + 1][1] / -2;
+                this.XChanger = 0;
+                this.currentPathPoint += 1;
+            }
+            
+        }
+        if (this.YChanger != 0)  {
+            
+            
+            if (this.position[1] > this.path[this.currentPathPoint ][0] && this.YChanger > 0) {
+                
+                this.XChanger = path[this.currentPathPoint + 1][1];
+                
+                this.YChanger = 0;
+                this.currentPathPoint += 1;
+            }
+            else if (this.position[1] < this.path[this.currentPathPoint ][0] && this.YChanger < 0) {
+                
+                this.XChanger = path[this.currentPathPoint + 1][1];
+                
+                this.YChanger = 0;
+                this.currentPathPoint += 1;
+            }
+            
+        }
+        
+
+        this.position[0] += this.movementSpeed*division*this.XChanger;
+        this.position[1] += this.movementSpeed*division*this.YChanger;
+        context.fillRect(this.position[0]*10, this.position[1]*10, 50, 50);
+        
+    }
+}
---
+++ Javascript/Tower.js
@@ -0,0 +1,10 @@
+class Tower {
+    constructor(position, damage, fireRate, range){
+        this.position = position;
+        this.damage = damage;
+        this.fireRate = fireRate;
+        this.range = range;
+
+        let rotation = 0;
+    }
+}
